08d758f05c4bf9ad8bd00bd2a430d6a46763639d
Master-Chen-Xin-Qi <77275457+Master-Chen-Xin-Qi@users.noreply.github.com>
2021-01-12 10:50:48 +0800

    Update pytorch_to_onnx.py

--- pytorch_to_onnx.py
+++ pytorch_to_onnx.py
@@ -2,29 +2,32 @@ import io
 import torch
 import torch.onnx
 from models.fusenet_model import FuseNet
-
+import torchvision
  
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
 def test():
-  #model = FuseNet(num_labels=6)
   
-  pthfile = './checkpoints/mobile_best_model_cpu_320x240.pth.tar'
-  model = torch.load(pthfile)
+  pthfile = './checkpoints/upsample_best_model_1.pth.tar'
+  model = torch.load(pthfile, map_location='cpu')
+  fusenet = FuseNet(num_labels = 6)
+  fusenet.load_state_dict(model['state_dict'],strict=True)
+  modnet = fusenet.cpu()
+  modnet.eval()
+  print('end!\n')
+  #convert_model(modnet)
+  rgb_inputs= torch.randn(1, 3, 320, 240)
+  depth_inputs = torch.randn(1, 1, 320, 240)
+  input_names = [ "rgb_inputs" ] + [ "depth_inputs" ]
+  output_names = [ "y"]
 
-  model.load_state_dict(loaded_model['state_dict'])
-  # model = model.to(device)
 
-  #data type nchw
-  dummy_input1 = torch.randn(1, 3, 240, 320)
-  dummy_input2 = torch.randn(1, 1, 240, 320)
-  # dummy_input2 = torch.randn(1, 3, 64, 64)
-  # dummy_input3 = torch.randn(1, 3, 64, 64)
-  input_names = [ "rgb", "depth"]
-  output_names = [ "output_class" ]
+  inputdata = (rgb_inputs, depth_inputs)
+  torch_out = torch.onnx.export(modnet, inputdata, "./mobile_best.onnx", export_params=True,opset_version=11, verbose=False, 
+                                input_names=input_names, output_names=output_names, keep_initializers_as_inputs=True,training=False)
+  #operator_export_type=torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK
 
-  # torch.onnx.export(model, (dummy_input1, dummy_input2, dummy_input3), "C3AE.onnx", verbose=True, input_names=input_names, output_names=output_names)
-  torch.onnx._export(model, (dummy_input1,dummy_input2), "FuseNet_test.onnx", opset_version = 9, verbose=True, input_names=input_names, output_names=output_names)
+  print('done!!')
 
 if __name__ == "__main__":
   test()
+
